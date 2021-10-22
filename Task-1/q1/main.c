000a91a8ffbd0b2ae2e14316a8fad03e86f08acf
ariel7342 <ariell@wix.com>
2021-10-22 13:41:09 +0300

    end of lesson

--- Task-1/q1/main.c
+++ Task-1/q1/main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 
+//define number of cols and rows
+#define NUMBER_OF_COLS 10
+#define NUMBER_OF_ROWS 10
+
+int countIndentionAmount(int num);
+
+void printMulBoard();
+
+// main - prints multiply board of 10
 int main() {
-    printf("Hello, World!\n");
+    printMulBoard();
     return 0;
+}
+
+//count how much indention needed and return it
+int countIndentionAmount(int num) {
+    int count = 0;
+    do {
+        num /= 10;
+        ++count;
+    } while (num != 0);
+    return count;
+}
+
+//print multiply board
+void printMulBoard() {
+    //loop from 1 to 10 for rows multiply, add a brake line in each iteration
+    for (int i = 1; i <= NUMBER_OF_COLS; i++) {
+        //loop from 1 to 10 for cols multiply, multiply col vs row value and prints it
+        for (int j = 1; j <= NUMBER_OF_ROWS; j++) {
+            //indention to right
+            int indentionAmount = countIndentionAmount(j * 10);
+            if (indentionAmount == 2) {
+                printf("%*d ", indentionAmount, j * i);
+//            } else {
+                printf("%*d", indentionAmount, j * i);
+            }
+
+        }
+        printf("\n");
+    }
 }
--- lesson 2/q1/main.c
+++ lesson 2/q1/main.c
@@ -13,26 +13,92 @@ char *findMaxLen(char **text, int size);
 
 void myFree(char **text, int size);
 
+void checkMemoryAllocation(void *ptr);
+
 int main() {
     char **text;
     int size;
     char *maxRow;
     size = readText(&text);
-    maxRow = findMaxLen(text, size);
-    printf("The longest row is: \"%s\"\n", maxRow);
-    myFree(text, size);
+    if (size != 0) {
+        maxRow = findMaxLen(text, size);
+        printf("The longest row is: \"%s\"\n", maxRow);
+        myFree(text, size);
+    }
+
     return 0;
 }
 
 
 int readText(char ***ptext) {
+    int logSize = 0, phsSize = INIT_SIZE;
+    char **text;
+
+    text = (char **) malloc(sizeof(char *) * INIT_SIZE);
+    checkMemoryAllocation(text);
+    char staticLine[MAX_LINE_LEN + INIT_SIZE];
+
+    // in cae you want a static size str
+    // char *dynamicLine = (char *) malloc(sizeof(char) * MAX_LINE_LEN + INIT_SIZE);
+
+    puts("Please enter sentences");
+    gets(staticLine);
+
+    if (staticLine[0] == '\0') {
+        free(text);
+        *ptext = NULL;
+        return 0;
+    }
+    // another way to check
+    //    while (strcmp(staticLine, "\0") != 0) {
+    //
+    //    }
+    while (staticLine[0] != '\0') {
+        if (logSize == phsSize) {
+            phsSize *= 2;
+            text = (char **) realloc(text, phsSize * sizeof(char *));
+            checkMemoryAllocation(text);
+        }
+        text[logSize] = (char *) malloc(strlen(staticLine) + 1 * sizeof(char));
+        checkMemoryAllocation(text[logSize]);
+        strcpy(text[logSize], staticLine);
+        logSize++;
+        gets(staticLine);
+    }
+    if (logSize < phsSize) {
+        text = (char **) realloc(text, logSize * sizeof(char *));
+        checkMemoryAllocation(text);
+    }
+
+    *ptext = text;
+    return phsSize;
 
 }
 
 char *findMaxLen(char **text, int size) {
+    int maxIndex = 0;
+    int i;
 
+    for (i = 1; i < size; i++) {
+        if (strlen(text[i]) > strlen(text[maxIndex])) {
+            maxIndex = i;
+        }
+    }
+
+    return text[maxIndex];
 }
 
 void myFree(char **text, int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        free(text[i]);
+    }
+    free(text);
+}
 
+void checkMemoryAllocation(void *ptr) {
+    if (ptr == NULL) {
+        printf("Memory Allocation Failed...\n");
+        exit(1);
+    }
 }
