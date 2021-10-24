58dfd4e63499c64abcd64ebe1627d7bc30aa6e59
ariel7342 <ariell@wix.com>
2021-10-24 22:40:11 +0300

    second solution for q1-a

--- Task-2/q1-a/main.c
+++ Task-2/q1-a/main.c
@@ -3,19 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-void mergeSort(int *array, int size);
+// merge sort main function
+void mergeSort(int **array, int size, int ascend_flag);
 
-int *mergeArrays(int *array1, int size1, int *array2, int size2);
+// merge to pointers arrays into one
+int **mergeArrays(int **array1, int size1, int **array2, int size2, int ascend_flag);
 
-void copyArray(int *des, int *src, int size);
+// copy pointers array
+void copyArray(int **des, int **src, int size);
 
+// sort pointers array
 int **pointerSort(int *arr, int size, int ascend_flag);
 
+// print pointers array values
 void printPointers(int **pointers, int size);
 
 #define SIZE 100
 
-
+// main function
 void main() {
     unsigned int size, i;
     int arr[SIZE];
@@ -36,36 +41,29 @@ void main() {
 }
 
 int **pointerSort(int *arr, int size, int ascend_flag) {
-    int *tmpArray = (int *) malloc(sizeof(int) * size);
     int **pointersArray = (int **) malloc(sizeof(int *) * size);
-    copyArray(tmpArray, arr, size);
-    mergeSort(tmpArray, size);
-
     for (int i = 0; i < size; i++) {
-        if (ascend_flag == 1) {
-            pointersArray[i] = &tmpArray[i];
-        } else {
-            pointersArray[i] = &tmpArray[size - 1 - i];
-        }
+        pointersArray[i] = &arr[i];
     }
+    mergeSort(pointersArray, size, ascend_flag);
 
     return pointersArray;
 }
 
-void printPointers(int **pointers, int size){
+void printPointers(int **pointers, int size) {
     for (int i = 0; i < size; i++) {
-            printf("%d ",*pointers[i]);
+        printf("%d ", *pointers[i]);
     }
 }
 
-void mergeSort(int *array, int size) {
-    int *tmpArray = NULL;
+void mergeSort(int **array, int size, int ascend_flag) {
+    int **tmpArray = NULL;
     if (size <= 1) {
         return;
     }
-    mergeSort(array, size / 2);
-    mergeSort(array + size / 2, size - size / 2);
-    tmpArray = mergeArrays(array, size / 2, array + size / 2, size - size / 2);
+    mergeSort(array, size / 2, ascend_flag);
+    mergeSort(array + size / 2, size - size / 2, ascend_flag);
+    tmpArray = mergeArrays(array, size / 2, array + size / 2, size - size / 2, ascend_flag);
     if (tmpArray) {
         copyArray(array, tmpArray, size);
         free(tmpArray);
@@ -74,39 +72,65 @@ void mergeSort(int *array, int size) {
     }
 }
 
-int *mergeArrays(int *array1, int size1, int *array2, int size2) {
+int **mergeArrays(int **array1, int size1, int **array2, int size2, int ascend_flag) {
     int index1, index2, indexResult;
-    int *tempArray = (int *) malloc(sizeof(int) * (size1 + size2));
+    int **tempArray = (int **) malloc(sizeof(int *) * (size1 + size2));
     if (tempArray == NULL) {
         printf("error");
     } else {
         index1 = index2 = indexResult = 0;
-        while ((index1 < size1) && (index2 < size2)) {
-            if (array1[index1] <= array2[index2]) {
+        if (ascend_flag == 1) {
+            while ((index1 < size1) && (index2 < size2)) {
+                if (*array1[index1] <= *array2[index2]) {
+                    tempArray[indexResult] = array1[index1];
+                    index1++;
+                } else {
+                    tempArray[indexResult] = array2[index2];
+                    index2++;
+                }
+                indexResult++;
+            }
+
+            while (index1 < size1) {
                 tempArray[indexResult] = array1[index1];
                 index1++;
-            } else {
+                indexResult++;
+            }
+            while (index2 < size2) {
                 tempArray[indexResult] = array2[index2];
                 index2++;
+                indexResult++;
             }
-            indexResult++;
+        } else {
+            while ((index1 < size1) && (index2 < size2)) {
+                if (*array1[index1] >= *array2[index2]) {
+                    tempArray[indexResult] = array1[index1];
+                    index1++;
+                } else {
+                    tempArray[indexResult] = array2[index2];
+                    index2++;
+                }
+                indexResult++;
+            }
+
+            while (index2 < size2) {
+                tempArray[indexResult] = array2[index2];
+                index2++;
+                indexResult++;
+            }
+
+            while (index1 < size1) {
+                tempArray[indexResult] = array1[index1];
+                index1++;
+                indexResult++;
+            }
         }
 
-        while (index1 < size1) {
-            tempArray[indexResult] = array1[index1];
-            index1++;
-            indexResult++;
-        }
-        while (index2 < size2) {
-            tempArray[indexResult] = array2[index2];
-            index2++;
-            indexResult++;
-        }
     }
     return tempArray;
 }
 
-void copyArray(int *des, int *src, int size) {
+void copyArray(int **des, int **src, int size) {
     int i;
     for (i = 0; i < size; i++) {
         des[i] = src[i];
