94e6a1203b288232bf43f5423796fb47a5a1a2ca
ariel7342 <ariell@wix.com>
2021-11-15 23:53:09 +0200

    finished t2-2

---
+++ Task-3/t2-2/cmake-build-debug/CMakeFiles/t2_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/t2_2.dir/main.c.o"
+  "t2_2"
+  "t2_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/t2_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
