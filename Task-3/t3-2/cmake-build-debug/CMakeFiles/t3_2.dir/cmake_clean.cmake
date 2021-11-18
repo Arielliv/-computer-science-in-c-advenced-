0c589fb277e5a0b5dc90a842eb9558780d6fad2c
ariel7342 <ariell@wix.com>
2021-11-18 09:22:46 +0200

    init t3-2

---
+++ Task-3/t3-2/cmake-build-debug/CMakeFiles/t3_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/t3_2.dir/main.c.o"
+  "t3_2"
+  "t3_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/t3_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Task-3/t3-2/cmake-build-debug/CMakeFiles/t3_2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for t3_2.
+# This may be replaced when dependencies are built.
