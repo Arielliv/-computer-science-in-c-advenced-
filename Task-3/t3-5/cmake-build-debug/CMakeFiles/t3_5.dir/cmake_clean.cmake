65251f86858d2262d483a6e3a434caa230002943
ariel7342 <ariell@wix.com>
2021-11-19 17:53:28 +0200

    init t3-5

---
+++ Task-3/t3-5/cmake-build-debug/CMakeFiles/t3_5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/t3_5.dir/main.c.o"
+  "t3_5"
+  "t3_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/t3_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
