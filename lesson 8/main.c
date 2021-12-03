aaffb76d3f26bcb5729c4a0b5ebbbf7c0d2705c4
ariel7342 <ariell@wix.com>
2021-12-03 12:35:41 +0200

    first part

--- lesson 8/main.c
+++ lesson 8/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#define SWAP(type, a, b) {type temp;temp =a; a=b; b=temp;}
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+// \ drop a line
+// #define SWAP(type, a, b) {type temp;\temp =1; a=b;\ b=temp;}
+
+int main(){
+    int x = 5;
+    int y = 6;
+    SWAP(int, x, y);
+    printf("x = %d, y = %d", x, y);
 }
