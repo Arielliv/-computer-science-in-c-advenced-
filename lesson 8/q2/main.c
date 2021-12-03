67987773687b6be55b1d4437d3f02ba3e75279e1
ariel7342 <ariell@wix.com>
2021-12-03 13:07:33 +0200

    finished 2

--- lesson 8/q2/main.c
+++ lesson 8/q2/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+    int var1;
+    float var2;
+    char op;
+    sscanf(argv[1], "%d", &var1);
+    // also can be done - var1 = atoi(argv[1]);
+    sscanf(argv[2], "%c", &op);
+    // also can be done - op = argv[2][0];
+    sscanf(argv[3], "%f", &var2);
+    printf("%d + %f = %.3f\n", var1, var2, var1 + var2);
     return 0;
 }
