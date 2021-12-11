b3b414ad1f21c6b34ca5d77c61e9890d70d579a3
ariel7342 <ariell@wix.com>
2021-12-11 12:20:34 +0200

    fix for 2

--- task 5/q-2/main.c
+++ task 5/q-2/main.c
@@ -4,6 +4,8 @@
 // argv[2] - second file name
 #include "files.h"
 
+// argv[1] - name of the file1
+// argv[2] - name of the file2
 int main(int argc, char *argv[]) {
     Exe5Q2(argv[1], argv[2]);
     return 0;
