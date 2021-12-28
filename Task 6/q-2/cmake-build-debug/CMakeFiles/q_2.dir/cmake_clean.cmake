f7fe0c60f8291b9cc19e1a34654638642cb1e09e
ariel7342 <ariell@wix.com>
2021-12-28 16:56:34 +0200

    almost finished q-2

---
+++ Task 6/q-2/cmake-build-debug/CMakeFiles/q_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/q_2.dir/main.c.o"
+  "q_2"
+  "q_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/q_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Task 6/q-2/main.c
@@ -0,0 +1,227 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef unsigned char BYTE;
+
+int *filter(int *Numbers, int size, BYTE *pred, int *new_size);
+
+BYTE setBit(BYTE ch, int i);
+
+int countSetBits(BYTE *bitsArr, int size);
+
+bool isBitOn(BYTE byte, int location);
+
+//int *reverseArray(int *arr, int size);
+int countFormTwoPred(BYTE *pred1, BYTE *pred2, int size);
+
+int *xorFilter(int *Numbers, int size, BYTE *pred1, BYTE *pred2, int *new_size);
+
+void main() {
+
+    int i, pr, new_size, size;
+
+    int *numbers;
+
+    BYTE *pred1, *pred2;
+
+    int *some_numbers1;
+
+    int *some_numbers2;
+
+
+    printf("Please enter the number of numbers:\n");
+
+
+    scanf("%d", &size);
+
+
+    numbers = (int *) malloc(size * sizeof(int));
+
+    if (numbers == NULL)
+
+        exit(1);
+
+
+    pred1 = (char *) malloc(size / 8 * sizeof(char));
+
+    if (pred1 == NULL)
+
+        exit(1);
+
+
+    pred2 = (char *) malloc(size / 8 * sizeof(char));
+
+    if (pred2 == NULL)
+
+        exit(1);
+
+
+    printf("Please enter the numbers:\n");
+
+
+    for (i = 0; i < size; i++)
+
+
+        scanf("%d", &numbers[i]);
+
+
+    printf("Please enter the first filter bits in hexa:\n");
+
+
+    for (i = 0; i < size / 8; i++) {
+
+        scanf("%x", &pr);
+
+        pred1[i] = (char) pr;
+
+    }
+
+
+    some_numbers1 = filter(numbers, size, pred1, &new_size);
+
+
+    printf("The numbers selected are:\n");
+
+
+    for (i = 0; i < new_size; i++)
+
+
+        printf("%d ", some_numbers1[i]);
+
+
+    printf("Please enter the second filter bits in hexa:\n");
+
+
+    for (i = 0; i < size / 8; i++) {
+
+
+        scanf("%x", &pr);
+
+
+        pred2[i] = (char) pr;
+
+
+    }
+
+
+    some_numbers2 = xorFilter(numbers, size, pred1, pred2, &new_size);
+
+
+    printf("The numbers selected are:\n");
+
+
+    for (i = 0; i < new_size; i++)
+
+
+        printf("%d ", some_numbers2[i]);
+
+
+    free(numbers);
+
+    free(some_numbers1);
+
+    free(pred1);
+
+    free(some_numbers2);
+
+    free(pred2);
+
+}
+
+int *filter(int *Numbers, int size, BYTE *pred, int *new_size) {
+    int i, j;
+    int count = countSetBits(pred, size / 8);
+    int counter = count - 1;
+    int *newNumbers = (int *) malloc(sizeof(int) * count);
+
+    for (j = (size / 8) - 1; j >= 0; j--) {
+        for (i = size; i >= 0; i--) {
+            bool checkPred1 = isBitOn(pred[j], i);
+            if (checkPred1) {
+                newNumbers[counter] = Numbers[(j + 1) * (i)];
+                counter--;
+            }
+        }
+//        newNumbers = reverseArray(newNumbers, count);
+    }
+
+    *new_size = count;
+    return newNumbers;
+}
+
+//int *reverseArray(int *arr, int size) {
+//    int i, y;
+//    int *reversedArray = (int *) malloc(sizeof(int) * size);
+//    for (y = 0, i = size - 1; i >= 0; y++, i--) {
+//        reversedArray[y] = arr[i];
+//    }
+//    return reversedArray;
+//}
+
+bool isBitOn(BYTE byte, int location) {
+    BYTE mask = 1 << location;
+    return byte & mask;
+}
+
+BYTE setBit(BYTE ch, int i) {
+    BYTE mask = 1 << i;
+    return ch | mask;
+}
+
+int countSetBits(BYTE *bitsArr, int size) {
+    int i;
+    BYTE currByte;
+    BYTE mask = 0x01;
+    int counter = 0;
+    for (i = 0; i < size; i++) {
+        currByte = bitsArr[i];
+        while (currByte != 0) {
+            if ((mask & currByte) == 0x01)
+                counter++;
+            currByte = currByte >> 0x01;
+        }
+    }
+    return counter;
+}
+
+int countFormTwoPred(BYTE *pred1, BYTE *pred2, int size) {
+    int counter = 0;
+    int i, j;
+    for (j = size / 8; j >= 0; j--) {
+        for (i = size; i >= 0; i--) {
+            bool checkPred1 = isBitOn(pred1[j], i);
+            bool checkPred2 = isBitOn(pred2[j], i);
+            if ((checkPred1 || checkPred2) && !(checkPred1 && checkPred2)) {
+                counter++;
+            }
+
+        }
+    }
+    return counter;
+}
+
+
+int *xorFilter(int *Numbers, int size, BYTE *pred1, BYTE *pred2, int *new_size) {
+    int i, j;
+    int count = countFormTwoPred(pred1, pred2, size);
+    int counter = count - 1;
+    int *newNumbers = (int *) malloc(sizeof(int) * count);
+
+    for (j = size / 8; j >= 0; j--) {
+        for (i = size; i >= 0; i--) {
+
+            bool checkPred1 = isBitOn(pred1[j], i);
+            bool checkPred2 = isBitOn(pred2[j], i);
+            if ((checkPred1 || checkPred2) && !(checkPred1 && checkPred2)) {
+                newNumbers[counter] = Numbers[(j + 1) * (i)];
+                counter--;
+            }
+
+        }
+//        newNumbers = reverseArray(newNumbers, logSize);
+    }
+
+    *new_size = count;
+    return newNumbers;
+}
